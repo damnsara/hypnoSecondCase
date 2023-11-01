f5c1dbe04b492fa859d5ee7d3f8a7d8c87ff5e09
damnsara <sara.iglesias0309@gmail.com>
2023-10-31 22:58:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using RestAPICase;
+
+HttpClient client = new HttpClient() { BaseAddress = new Uri("https://jsonplaceholder.typicode.com") };
+var response = await client.GetAsync("comments");
+var content = await response.Content.ReadAsStringAsync();
+var comments = JsonConvert.DeserializeObject<List<Comment>>(content);
+
+
+List<Comment> sortByDecreasingOrder(List<Comment> list) {
+
+    List<Comment> sortedComments = list.OrderByDescending(comment => comment.PostId).ThenByDescending(comment => comment.Body).ToList();
+
+    return sortedComments;
+}
+
+
+void topComments(List<Comment> list, int limit) {
+    for (int i = 0; i < limit; i++)
+    {
+
+        Console.WriteLine(list[i].PostId);
+    }
+}
+
+sortByDecreasingOrder(comments);
+topComments(sortByDecreasingOrder(comments), 10);
+
+
